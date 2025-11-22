6fef7161e1982a6cf2131df6058939dd5ed07b80
Goricaa <gavricgoricaa@yahoo.com>
2025-11-22 19:39:48 +0100

    Izmijenjen Program.java i dodan ThreeDemo

--- src/main/java/com/academy/Program.java
+++ src/main/java/com/academy/Program.java
@@ -3,5 +3,6 @@ package com.academy;
 public class Program {
     public static void main(String[] args) {
         System.out.println("Vozdra raja, pozdrav iz Intellij");
+        System.out.println("Jeste mi dobro, kako ste?");
     }
 }
