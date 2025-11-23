fe301a76049b17662628a40566f3aae9044ee571
Goricaa <gavricgoricaa@yahoo.com>
2025-11-23 21:11:48 +0100

    Rezime, tipovi podataka.

---
+++ src/main/java/com/academy/five/FiveDemo.java
@@ -0,0 +1,32 @@
+package com.academy.five;
+/**
+* TIP PODATKA varijabla =  vrijednost
+* float floatNumber = 3.4F Java ce ga tretirati kao 32bitni zapis sa F
+ *
+ *  TIPOVI PODATAKA
+ *1. PROSTI
+ * -CIJELI BROJEVI: byte, short, int (DEFAULT), long
+ * -DECIMALNI: float i double
+ * - CHAR ili chars: char ' '
+ * - boolean, true ili false
+ * ukupno ih je 8
+ *
+ * 2. SLOZENI ILI OBJEKTNI
+ * - ARITHMETIC: +,-,*,/, % (MODULO ILI OSTATAK OD DJELJENJA)
+ * - UNARNI: +,- (poz i neg brojevi), ++, --, komplement
+ * - POREDJENJA I JEDNAKOSTI: ==, !=, >, >=, <, <=
+ * - USLOVNI OPERATORI: && AND, || OR, TERNARNI ILI USLOV
+ * - OPERATORI NA NIVOU BITA: SIFTANJE <<1, >>2, ~ OVO JE TILDA, UNARNI KOMPLEMENT
+ *
+ * ZAPIS VRIJEDNOSTI PODATKA MOZE BITI U:
+ * 1. DEKADNI,
+ * 2. BINARNI,
+ * 3. hex tj HEKSADECIMALNI
+ *
+*/
+public  class FiveDemo {
+    public static void main(String[] args) {
+
+    }
+
+}
