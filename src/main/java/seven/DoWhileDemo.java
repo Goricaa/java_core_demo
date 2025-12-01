053012aa8c676b8475676117d9f51fb2830cfb87
Goricaa <gavricgoricaa@yahoo.com>
2025-12-01 21:29:46 +0100

    Loops introduction - infinite loop, dead loop

---
+++ src/main/java/seven/DoWhileDemo.java
@@ -0,0 +1,13 @@
+package seven;
+
+
+public class DoWhileDemo {
+    public static void main(String[] args) {
+        int number = 11;
+//        while(condition){blok koda}
+        do{
+            System.out.println(number);
+            number--;
+        }while (number>0);
+    }
+}
---
+++ src/main/java/seven/InfinitiveLoop.java
@@ -0,0 +1,14 @@
+package seven;
+
+public class InfinitiveLoop {
+    public static void main(String[] args) {
+
+        //        while (true){
+//            System.out.println("Izlazim iz petlje nikad...");
+//        }
+
+//        for (; ; ) {
+//            System.out.println("Nikada ne izađeš iz petlje...");
+//        }
+    }
+}
