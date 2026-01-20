f199223dddaca0186c9247aaed7e6c789995565b
Goricaa <gavricgoricaa@yahoo.com>
2026-01-20 14:18:21 +0100

    OOP Object Oriented Programming

---
+++ src/main/java/com/academy/ten/animal/Cat.java
@@ -0,0 +1,13 @@
+package com.academy.ten.animal;
+
+public class Cat extends Animal {
+    public Cat(String name) {
+        super(name);
+    }
+
+    @Override
+    public void playSound() {
+        System.out.println("Mjau Mjau Mjau...");
+    }
+}
+
---
+++ src/main/java/com/academy/ten/animal/Fish.java
@@ -0,0 +1,13 @@
+package com.academy.ten.animal;
+
+public class Fish extends Animal {
+    public Fish(String name) {
+        super(name);
+    }
+
+    @Override
+    public void playSound() {
+        System.out.println("Br Br Br...");
+    }
+}
+
