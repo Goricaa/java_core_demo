c0631801a249722bdf60b3b2fa8219eadb9ff3e8
Goricaa <gavricgoricaa@yahoo.com>
2026-01-20 13:27:54 +0100

    OOP Object Oriented Programming

---
+++ src/main/java/com/academy/eight/oop/Demo.java
@@ -0,0 +1,23 @@
+package com.academy.eight.oop;
+
+public class Demo {
+    public static void main(String[] args) {
+        int number = 23;
+        number = 13;
+
+        int[] numbers = new int[3];
+        numbers[0] = 23;
+        numbers[1] = 13;
+        numbers[2] = 10;
+
+        Person person = new Person();
+        person.nin = 12134;
+        person.name = "Aleksandar";
+        person.surname = "Petković";
+
+        // Otkud nama hashCode() funkcija
+        int hashCode = person.hashCode();
+        String personString = person.toString();
+        System.out.println(hashCode + " --> " + personString);
+    }
+}
---
+++ src/main/java/com/academy/eight/oop/OrderItem.java
@@ -0,0 +1,6 @@
+package com.academy.eight.oop;
+
+public class OrderItem {
+    private int id;
+    private String nameOfArticle;
+}
